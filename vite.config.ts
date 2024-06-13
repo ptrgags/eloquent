db937ce6f5b61cd89b5331c5576a8b029c71c6d2
Peter Gagliardi <ptrgags@gmail.com>
2024-06-13 10:32:14 -0400

    Configure Eloquent as a PWA app

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,51 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+
+import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      manifest: {
+        name: 'Eloquent',
+        short_name: 'Eloquent',
+        description: 'Rank ideas using an Elo rating system',
+        theme_color: '#1eb192',
+        icons: [
+          {
+            src: 'maskable_icon_x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'maskable_icon_x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'maskable_icon_x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'maskable_icon_x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: "/eloquent/"
+  base: '/eloquent/'
 })
